1999805d176a70332ca326b5f63060519da5c3be
Damian Jamróz <mrlorem@gmail.com>
2022-05-26 00:23:31 +0200

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+const amountInput = document.querySelector("#amount-input");
+const submitBtn = document.querySelector("#submit-btn");
+const euroOpt = document.querySelector("#euro-opt");
+const usdOpt = document.querySelector("#usd-opt");
+const frankOpt = document.querySelector("#frank-opt");
+
+let span = document.querySelector("span");
+
+submitBtn.addEventListener("click", function () {});
