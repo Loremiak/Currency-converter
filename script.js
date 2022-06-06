18602f5c4fa23b54630efd923afc84a193ad270f
Damian Jamróz <mrlorem@gmail.com>
2022-06-06 11:31:00 +0200

    design and some js work

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const amountInput = document.querySelector("#amount-input");
+const selectDOM = document.querySelector("#select-dom");
 const submitBtn = document.querySelector("#submit-btn");
-const euroOpt = document.querySelector("#euro-opt");
-const usdOpt = document.querySelector("#usd-opt");
-const frankOpt = document.querySelector("#frank-opt");
 
 let span = document.querySelector("span");
+
+function getCurrencyList() {
+	fetch("https://api.frankfurter.app/latest?from=PLN")
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			let currencies = data.rates;
+
+			selectDOM.addEventListener("change", () => {
+				// body.appendChild(h1);
+				// h1.innerText = `Wybrałeś ${event.target.value} o wartości ${
+				// 	currencies[event.target.value]
+				// } zł`;
+				console.log(event.target.value);
+			});
+			Object.keys(data.rates).forEach((element) => {
+				let option = document.createElement("option");
+				// option.setAttribute("value", element);
+				option.textContent = element;
+				selectDOM.appendChild(option);
+			});
+			document.body.appendChild(selectDOM);
+		});
+}
+getCurrencyList();
 
 submitBtn.addEventListener("click", function () {});
