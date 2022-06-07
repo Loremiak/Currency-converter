c4987b567f5bd1103dfda57f776ae243f3da649f
Damian Jamróz <mrlorem@gmail.com>
2022-06-07 18:04:10 +0200

    cloud msg, sound for btn

--- script.js
+++ script.js
@@ -2,17 +2,33 @@ const amountInput = document.querySelector("#amount-input");
 const selectDOM = document.querySelector("#select-dom");
 const submitBtn = document.querySelector("#submit-btn");
 
+const main = document.querySelector("main");
+const wheatleyImg = document.querySelector(".wheatley-img");
+
 let span = document.querySelector("span");
 
+wheatleyImg.addEventListener("mouseover", () => {
+	const cloud = document.createElement("span");
+	cloud.setAttribute("class", "cloud");
+	cloud.innerText =
+		"Hej! Jestem Wheatley, tą stronę stworzył Damian J. Inspirował się grą Portal 2, a więc uważaj na GLaDOS!";
+	main.appendChild(cloud);
+
+	const counter = () => {
+		main.removeChild(cloud);
+	};
+	setTimeout(counter, 6000);
+});
+
 function getCurrencyList() {
-	fetch("https://api.frankfurter.app/latest?from=PLN")
+	fetch("http://api.nbp.pl/api/exchangerates/tables/a/")
 		.then((response) => {
 			return response.json();
 		})
 		.then((data) => {
 			let currencies = data.rates;
 
-			selectDOM.addEventListener("change", () => {
+			selectDOM.addEventListener("change", (event) => {
 				// body.appendChild(h1);
 				// h1.innerText = `Wybrałeś ${event.target.value} o wartości ${
 				// 	currencies[event.target.value]
@@ -21,13 +37,20 @@ function getCurrencyList() {
 			});
 			Object.keys(data.rates).forEach((element) => {
 				let option = document.createElement("option");
-				// option.setAttribute("value", element);
 				option.textContent = element;
 				selectDOM.appendChild(option);
 			});
-			document.body.appendChild(selectDOM);
 		});
 }
 getCurrencyList();
 
-submitBtn.addEventListener("click", function () {});
+function exchange(e) {
+	e.preventDefault();
+	if (amountInput.value > 0) {
+		document.getElementById("play1").play();
+	} else {
+		document.getElementById("play2").play();
+	}
+}
+
+submitBtn.addEventListener("click", exchange);
